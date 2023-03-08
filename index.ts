2c64d056791b2135d573a42bc659b6bb9260a9ec
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-08 14:43:26 +0900

    postされたユーザー情報をデータベースに保存

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 const app: express.Express = express();
 const prisma = new PrismaClient();
+app.use(express.json());
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("こんにち");
@@ -10,6 +11,17 @@ app.get("/", (req: express.Request, res: express.Response) => {
 app.get("/users", async (req: express.Request, res: express.Response) => {
   const users = await prisma.user.findMany();
   return res.json(users);
+});
+
+app.post("/users", async (req: express.Request, res: express.Response) => {
+  const { id, name } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      id: id,
+      name: name,
+    },
+  });
+  return res.json(user);
 });
 
 app.listen(3000, () => {

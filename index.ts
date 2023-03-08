8e3bb78a7cc451e43ed32b13867fb2f435d3d5e7
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-08 13:47:18 +0900

    mySQLへの接続確認

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 const app: express.Express = express();
+const prisma = new PrismaClient();
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("こんにち");
 });
+
+app.get("/users", async (req: express.Request, res: express.Response) => {
+  const users = await prisma.user.findMany();
+  return res.json(users);
+});
+
 app.listen(3000, () => {
   console.log("3000起動");
 });

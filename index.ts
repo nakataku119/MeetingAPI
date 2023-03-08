9f7f53abc04fcedbc0850be4c54e2702b59fd377
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-08 16:11:09 +0900

    User情報の更新機能追加

--- index.ts
+++ index.ts
@@ -3,22 +3,35 @@ import { PrismaClient } from "@prisma/client";
 const app: express.Express = express();
 const prisma = new PrismaClient();
 app.use(express.json());
-
+// topページへアクセス
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("hello");
 });
-
+// User一覧の取得
 app.get("/users", async (req: express.Request, res: express.Response) => {
   const users = await prisma.user.findMany();
   return res.json(users);
 });
-
+// Userデータの追加
 app.post("/users", async (req: express.Request, res: express.Response) => {
   const { id, name } = req.body;
   const user = await prisma.user.create({
     data: {
       id: id,
       name: name,
+    },
+  });
+  return res.json(user);
+});
+// Userデータの更新
+app.put("/users/:id", async (req: express.Request, res: express.Response) => {
+  const { id, name } = req.body;
+  const user = await prisma.user.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
     },
   });
   return res.json(user);

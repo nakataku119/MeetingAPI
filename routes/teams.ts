19f98fe8782c10e23bd4a36e4aad7ae85a2af36a
nakataku119 <t.nakahara.1109@icloud.com>
2023-03-13 18:30:17 +0900

    チーム更新時に所属メンバーも更新

--- routes/teams.ts
+++ routes/teams.ts
@@ -32,16 +32,35 @@ router.get("/teams/:id", async (req: Request, res: Response) => {
   });
   return res.json(team);
 });
-// チームの更新
+// 関連するユーザーの呼び出し
+router.get("/teams/:id/users", async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const team_users = await prisma.team.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      users: true,
+    },
+  });
+  return res.json(team_users);
+});
+// チームの更新、同時に所属するユーザーも更新
 router.put("/teams/:id", async (req: Request, res: Response) => {
   const id = Number(req.params.id);
-  const { name } = req.body;
+  const { name, userId } = req.body;
   const team = await prisma.team.update({
     where: {
       id,
     },
     data: {
       name,
+      users: {
+        // 既存メンバーがリセットされる
+        set: [{ id: userId }],
+        // 既存メンバーに追加する
+        // connect: [{ id: userId }]
+      },
     },
   });
   return res.json(team);
@@ -55,19 +74,6 @@ router.delete("/teams/:id", async (req: Request, res: Response) => {
     },
   });
   return res.json(team);
-});
-// 関連するユーザーの呼び出し
-router.get("/teams/:id/users", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const team_users = await prisma.team.findUnique({
-    where: {
-      id,
-    },
-    include: {
-      users: true,
-    },
-  });
-  return res.json(team_users);
 });
 
 export default router;

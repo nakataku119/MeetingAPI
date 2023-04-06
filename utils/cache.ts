70bd08aa62046b851cf2ce6df5cb24b0353595cd
nakataku119 <t.nakahara.1109@icloud.com>
2023-04-06 11:30:18 +0900

    cacheの設定

---
+++ utils/cache.ts
@@ -0,0 +1,11 @@
+import NodeCache from "node-cache";
+
+const cache = new NodeCache({ stdTTL: 180 });
+
+export const getValueFromCache = (key: string): any | null => {
+  return cache.get(key);
+};
+
+export const saveValueToCache = (key: string, value: any): void => {
+  cache.set(key, value);
+};

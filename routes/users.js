8b2120cc2320d690009516f8fb0907ef8419d06d
nakataku119 <t.nakahara.1109@icloud.com>
2023-04-06 14:59:46 +0900

    バージョンの更新

---
+++ routes/users.js
@@ -0,0 +1,61 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const client_1 = require("@prisma/client");
+const userId_1 = require("../utils/userId");
+const router = (0, express_1.Router)();
+const prisma = new client_1.PrismaClient();
+router.get("/users/me", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const currentUser = yield prisma.user.findUnique({
+        where: { id: (0, userId_1.getUserIdFromCache)() },
+        include: {
+            mtgs: {
+                include: { agendas: true, users: { where: { deleted: false } } },
+            },
+            teams: {
+                where: { deleted: false },
+                include: {
+                    users: {
+                        where: { NOT: { id: (0, userId_1.getUserIdFromCache)() }, deleted: false },
+                    },
+                },
+            },
+        },
+    });
+    return res.json(currentUser);
+}));
+router.post("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name } = req.body;
+    const user = yield prisma.user.create({
+        data: {
+            id: (0, userId_1.getUserIdFromCache)(),
+            name: name,
+        },
+    });
+    return res.json(user);
+}));
+router.put("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name } = req.body;
+    if (!name) {
+        return;
+    }
+    const user = yield prisma.user.update({
+        where: {
+            id: (0, userId_1.getUserIdFromCache)(),
+        },
+        data: {
+            name,
+        },
+    });
+    return res.json(user);
+}));
+exports.default = router;

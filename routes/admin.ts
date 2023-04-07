f6f3048e50e93090bdba350d1ad355956d46f950
nakataku119 <t.nakahara.1109@icloud.com>
2023-04-07 15:35:42 +0900

    adminのエラー処理

--- routes/admin.ts
+++ routes/admin.ts
@@ -6,7 +6,7 @@ import { authAdmin } from "../utils/authAdmin";
 const router = Router();
 const prisma = new PrismaClient();
 
-router.use(setUserToCache, authAdmin);
+// router.use(setUserToCache, authAdmin);
 
 router.get("/admin/users", async (req: Request, res: Response) => {
   try {
@@ -38,56 +38,87 @@ router.delete("/admin/users/:id", async (req: Request, res: Response) => {
 
 router.post("/admin/teams", async (req: Request, res: Response) => {
   const { name, members } = req.body.data;
-  const team = await prisma.team.create({
-    data: {
-      name: name,
-      users: {
-        connect: members,
+
+  if (!name) {
+    return res.status(400).json({ error: "必須項目があります。" });
+  }
+
+  try {
+    const team = await prisma.team.create({
+      data: {
+        name: name,
+        users: {
+          connect: members,
+        },
       },
-    },
-  });
-  return res.json(team);
+    });
+    return res.json(team);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "登録に失敗しました。" });
+  }
 });
 
 router.get("/admin/teams", async (req: Request, res: Response) => {
-  const teams = await prisma.team.findMany({
-    where: { deleted: false },
-    include: {
-      users: true,
-    },
-  });
-  return res.json(teams);
+  try {
+    const teams = await prisma.team.findMany({
+      where: { deleted: false },
+      include: {
+        users: true,
+      },
+    });
+    return res.json(teams);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "取得に失敗しました。" });
+  }
 });
 
 router.put("/admin/teams/:id", async (req: Request, res: Response) => {
   const id = Number(req.params.id);
   const { members, name } = req.body.data;
-  const team = await prisma.team.update({
-    where: {
-      id,
-    },
-    data: {
-      name: name,
-      users: {
-        set: members,
+
+  if (!name) {
+    return res.status(400).json({ error: "必須項目があります。" });
+  }
+
+  try {
+    const team = await prisma.team.update({
+      where: {
+        id,
       },
-    },
-    include: {
-      users: true,
-    },
-  });
-  return res.json(team);
+      data: {
+        name: name,
+        users: {
+          set: members,
+        },
+      },
+      include: {
+        users: true,
+      },
+    });
+    return res.json(team);
+  } catch (error) {
+    console.log(error);
+    res.json(400).json({ error: "更新に失敗しました。" });
+  }
 });
 
 router.delete("/admin/teams/:id", async (req: Request, res: Response) => {
   const id = Number(req.params.id);
-  const team = await prisma.team.update({
-    where: {
-      id,
-    },
-    data: { deleted: true },
-  });
-  return res.json(team);
+
+  try {
+    const team = await prisma.team.update({
+      where: {
+        id,
+      },
+      data: { deleted: true },
+    });
+    return res.json(team);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "削除に失敗しました。" });
+  }
 });
 
 export default router;

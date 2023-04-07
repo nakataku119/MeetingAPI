58db1b5d30dd62af8e48a17c54900c3090839d0a
nakataku119 <t.nakahara.1109@icloud.com>
2023-04-07 14:07:09 +0900

    update時のエラー処理追加

--- routes/meetings.ts
+++ routes/meetings.ts
@@ -29,39 +29,50 @@ router.post("/mtgs", async (req: Request, res: Response) => {
     });
     return res.json(mtg);
   } catch (error) {
+    console.log(error);
     return res.status(400).json({ error: "データの登録に失敗しました。" });
   }
 });
 
 router.put("/mtgs/:id", async (req: Request, res: Response) => {
   const id = Number(req.params.id);
-  const { schedule, users, agendas, teamId, freeAgenda } = req.body.data;
-  const mtg = await prisma.mtg.update({
-    where: {
-      id,
-    },
-    data: {
-      schedule: schedule,
-      freeAgenda: freeAgenda,
-      users: {
-        set: users,
+  const { schedule, users, agendas, teamId, freeAgenda } = req.body;
+
+  if (!schedule || !teamId) {
+    return res.status(400).json({ error: "必須項目が入力されていません。" });
+  }
+
+  try {
+    const mtg = await prisma.mtg.update({
+      where: {
+        id,
       },
-      agendas: {
-        create: agendas,
-      },
-      team: {
-        connect: {
-          id: teamId,
+      data: {
+        schedule: schedule,
+        freeAgenda: freeAgenda,
+        users: {
+          set: users,
+        },
+        agendas: {
+          create: agendas,
+        },
+        team: {
+          connect: {
+            id: teamId,
+          },
         },
       },
-    },
-    include: {
-      team: true,
-      users: true,
-      agendas: true,
-    },
-  });
-  return res.json(mtg);
+      include: {
+        team: true,
+        users: true,
+        agendas: true,
+      },
+    });
+    return res.json(mtg);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "データの登録に失敗しました。" });
+  }
 });
 
 router.delete("/mtgs/:id", async (req: Request, res: Response) => {

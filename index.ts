13d340d652a8b04c8bc15083ccac8206b2a9af4d
nakataku119 <t.nakahara.1109@icloud.com>
2023-04-11 14:41:52 +0900

    不要なコードの削除

--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
-import express, { Express, NextFunction, Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import usersRoutes from "./routes/users";
 import agendaRoutes from "./routes/agendas";
 import meetingRoutes from "./routes/meetings";
 import adminRoutes from "./routes/admin";
-import { setUserIdToCache } from "./utils/userId";
 import { checkJwt } from "./utils/auth";
 import cors from "./utils/cors";
-import { deleteCache } from "./utils/cache";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger";
+import { setUserIdToReq } from "./utils/userId";
 
 const app: Express = express();
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 /**
  * @swagger
@@ -25,31 +24,10 @@ app.use(express.json());
  *        description: "top response"
  */
 app.get("/", (req: Request, res: Response) => {
-  res.json("top response");
+  res.json("root response");
 });
 
-app.use("/*", cors, checkJwt, setUserIdToCache);
-
-/**
- * @swagger
- * /logout:
- *  post:
- *    description: ログインユーザーのキャッシュを削除
- *    responses:
- *      200:
- *        description: 成功
- *        content:
- *          text/plain:
- *            schema:
- *              type: string
- *              example: キャッシュを削除しました。
- */
-app.post("/logout", (req: Request, res: Response) => {
-  console.log("logout");
-  deleteCache();
-  res.json("キャッシュを削除しました。");
-});
-
+app.use("/", cors, checkJwt, setUserIdToReq);
 app.use("/", usersRoutes, agendaRoutes, meetingRoutes, adminRoutes);
 
 app.listen(8080, () => {

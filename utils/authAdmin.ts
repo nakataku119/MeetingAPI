a07158b46ade33ea21b68fdf3d05d9448c89441a
nakataku119 <t.nakahara.1109@icloud.com>
2023-04-11 14:37:26 +0900

    userIdの保持を変更

--- utils/authAdmin.ts
+++ utils/authAdmin.ts
@@ -1,13 +1,28 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response, NextFunction } from "express";
-import { getUserFromCache } from "./currentUser";
+import { errorHandle } from "./errorHandle";
 
-export const authAdmin = (req: Request, res: Response, next: NextFunction) => {
-  const currentUser = getUserFromCache();
-  if (currentUser.admin) {
-    next();
-  } else {
-    const error: Error & { status?: number } = new Error("権限がありません。");
-    error.status = 403;
-    next(error);
+const prisma = new PrismaClient();
+
+export const authAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const currentUser = await prisma.user.findUnique({
+      where: { id: req.body.sub },
+    });
+    if (currentUser?.admin) {
+      next();
+    } else {
+      const error: Error & { status?: number } = new Error(
+        "権限がありません。"
+      );
+      error.status = 403;
+      next(error);
+    }
+  } catch (error: any) {
+    errorHandle(error, res);
   }
 };

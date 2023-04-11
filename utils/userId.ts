854cf416b9ee803671a6b1c322cd181962ef79b0
nakataku119 <t.nakahara.1109@icloud.com>
2023-04-11 14:39:32 +0900

    ユーザーIDの取得方法を変更

--- utils/userId.ts
+++ utils/userId.ts
@@ -1,22 +1,14 @@
 import { Request, Response, NextFunction } from "express";
-import { getValueFromCache, saveValueToCache } from "./cache";
 import jwt_decode from "jwt-decode";
 
-export const setUserIdToCache = (
+export const setUserIdToReq = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (!getUserIdFromCache()) {
-    console.log("set user id cache");
-    const decodedJwt: { sub: string } = jwt_decode(
-      req.headers["authorization"]!.split(" ")[1]
-    );
-    saveValueToCache("userId", decodedJwt.sub);
-  }
+  const decodedJwt: { sub: string } = jwt_decode(
+    req.headers["authorization"]!.split(" ")[1]
+  );
+  req.body.id = decodedJwt.sub;
   next();
-};
-
-export const getUserIdFromCache = () => {
-  return getValueFromCache("userId");
 };
